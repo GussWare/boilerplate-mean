d60a1fd4942e49540a086691a0cc7cc15e737142
GussWare <gussware@gmail.com>
2021-06-27 00:41:37 -0500

    Cambios en constantes

--- src/v1/config/constants.config.js
+++ src/v1/config/constants.config.js
@@ -1,13 +1,31 @@
 export default {
 	NODE_ENV_DEVELOPER: "development",
 	NODE_ENV_PRODUCTION: "production",
-	FOLDER_TEM: "./src/tmp",
-	FOLDER_USERS: "./src/v1/files/users",
+
+	FOLDERS: {
+		TEMP: "./src/tmp",
+		USERS: "./src/v1/files/users",
+	},
 
 	TOKEN: {
 		TYPE_ACCESS: "access",
 		TYPE_REFRESH: "refresh",
 		TYPE_RESET_PASSWORD: "resetPassword",
 		TYPE_VERIFY_EMAIL: "verifyEmail",
+	},
+
+	WEB:{
+		TEMP: "tmp",
+		FILES: {
+			USERS: "files/users",
+		},
+		ASSETS: {
+			IMG:"assets/img"
+		}
+	},
+
+	IMG: {
+		NO_IMG: "no-img.jpg",
+		NO_IMG_THUMBNAIL: "thumbnail-no-img.jpg",
 	},
 };
