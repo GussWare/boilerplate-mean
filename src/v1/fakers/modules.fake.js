4619dfe18757b5d4363d25fe4e60b99bbdfb6377
GussWare <gussware@gmail.com>
2021-10-03 15:28:11 -0500

    se agrego informacion fake para modulos

--- src/v1/fakers/modules.fake.js
+++ src/v1/fakers/modules.fake.js
@@ -1,18 +1,65 @@
 import faker from "faker";
 import * as moduleService from "../services/modules/module.service";
+import * as actionService from "../services/actions/actions.service";
 
 export const moduleInitFaker = async () => {
 	let contaInserts = 0;
 
-	for (let i = 0; i < 100; i++) {
-		await moduleService.createModule({
-			name: faker.lorem.word(),
-			slug:faker.lorem.slug(),
-            guard:faker.lorem.slug(),
-            description:faker.lorem.paragraph()
+	const userModule = await moduleService.createModule({
+		name: "Usuarios",
+		slug: "users",
+		guard: "",
+		description: "Catálogo de usuarios",
+	});
+
+	if (userModule) {
+		await actionService.createAction({
+			name: "Listado de Usuarios",
+			slug: "users-list",
+			guard: "",
+			description: "Listado de usuarios",
+			module: userModule.id
 		});
 
-		contaInserts++;
+		await actionService.createAction({
+			name: "Crear Usuario",
+			slug: "users-create",
+			guard: "",
+			description: "Crear un nuevo usuario",
+			module: userModule.id
+		});
+
+		await actionService.createAction({
+			name: "Actualizar Usuario",
+			slug: "users-update",
+			guard: "",
+			description: "Actualizar usuario",
+			module: userModule.id
+		});
+
+		await actionService.createAction({
+			name: "Eliminar Usuario",
+			slug: "users-delete",
+			guard: "",
+			description: "Actualizar usuario",
+			module: userModule.id
+		});
+
+		await actionService.createAction({
+			name: "Habilitar Usuario",
+			slug: "users-enabled",
+			guard: "",
+			description: "Habilitar usuario",
+			module: userModule.id
+		});
+
+		await actionService.createAction({
+			name: "Deshabilitar Usuario",
+			slug: "users-disabled",
+			guard: "",
+			description: "Deshabilitar usuario",
+			module: userModule.id
+		});
 	}
 
 	return contaInserts;
