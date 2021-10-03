0b589c7f0de73da97ac52da401b0be82f9ff06bb
GussWare <gussware@gmail.com>
2021-10-03 11:57:01 -0500

    Se agrego test para probar la libreria de polyglot

--- src/v1/controllers/test.controller.js
+++ src/v1/controllers/test.controller.js
@@ -1,8 +1,8 @@
 import constants from "../config/constants.config";
 import catchAsyncHelper from "../helpers/catch.async.helper";
-import loggerHelper from "../helpers/logger.helper";
 import initFaker from "../fakers/init.faker";
 import fs from "fs-extra";
+
 
 export const testFs = catchAsyncHelper(async (req, res) => {
 	const path = constants.FOLDERS.USERS + "/1";
@@ -18,5 +18,11 @@ export const faker = catchAsyncHelper(async (req, res) => {
 
 	res.send({
 		message: "Faker complete",
+	});
+});
+
+export const polyglot = catchAsyncHelper(async(req, res) => {
+	res.send({
+		message: global.polyglot.t("AUTH_EMAIL_VERIFICATION_FAILED")
 	});
 });
